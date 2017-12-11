771d6ee6281d17ccd15c59a1f3ed46080626d743
PATIDELL\User <patrwol552@student.polsl.pl>
2017-12-11 16:58:17 +0100

    dodanie funkcji

--- app/src/main/java/com/example/user/pum_lab2/MainActivity.java
+++ app/src/main/java/com/example/user/pum_lab2/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.user.pum_lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void PobierzDane (View view) {
+        Button listamiejsc = (Button) findViewById(R.id.listamiejsc);
+        Button zmianamiejsca = (Button) findViewById(R.id.zmianamiejsca);
+        TextView miasto = (TextView) findViewById(R.id.miasto);
+        TextView dodatkowe_info = (TextView) findViewById(R.id.dodatkowe_info);
+        EditText podanemiejsce = (EditText) findViewById(R.id.podanemiejsce);
+
+        
+
+
+        if(zmianamiejsca.isPressed())
+        {
+            miasto.setText(podanemiejsce.getText());
+        }
+    }
+
 }
