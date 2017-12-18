ef3e64f82d228c23e6c5fb57e9154f3e6400cc85
PATIDELL\User <patrwol552@student.polsl.pl>
2017-12-18 16:04:09 +0100

    dodanie BindView, skreślenie findViewById

--- app/src/main/java/com/example/user/pum_lab2/MainActivity.java
+++ app/src/main/java/com/example/user/pum_lab2/MainActivity.java
@@ -9,28 +9,41 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity
 {
 
-    Button lista_miejsc_btn;
-    Button zmien_miejsce_btn;
-    TextView miasto;
-    TextView dodatkowe_info;
-    EditText podanemiejsce;
+    //Button lista_miejsc_btn;
+    //Button zmien_miejsce_btn;
+    //TextView miasto;
+    //TextView dodatkowe_info;
+    //EditText podanemiejsce;
 
+    @BindView(R.id.lista_miejsc_btn)
+    Button lista_miejsc_btn;
+
+    @BindView(R.id.zmien_miejsce_btn)
+    Button zmien_miejsce_btn;
+
+    @BindView(R.id.miasto)
+    TextView miasto;
+
+    @BindView(R.id.dodatkowe_info)
+    TextView dodatkowe_info;
+
+    @BindView(R.id.podanemiejsce)
+    EditText podanemiejsce;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lista_miejsc_btn = (Button) findViewById(R.id.lista_miejsc_btn);
-        zmien_miejsce_btn = (Button) findViewById(R.id.zmien_miejsce_btn);
-        miasto = (TextView) findViewById(R.id.miasto);
-        dodatkowe_info = (TextView) findViewById(R.id.dodatkowe_info);
-        podanemiejsce = (EditText) findViewById(R.id.podanemiejsce);
-
+        ButterKnife.bind(this);
 
         final Intent i = new Intent(this, ActivityOfCities.class);
 
@@ -65,8 +78,6 @@ public class MainActivity extends AppCompatActivity
 
 
     }
-
-
 
 
 }
