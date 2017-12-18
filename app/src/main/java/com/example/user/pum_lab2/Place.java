91978e9bc57790f499f417c95a96cb830d596a98
PATIDELL\User <patrwol552@student.polsl.pl>
2017-12-18 16:36:30 +0100

    dodanie klasy, pól, właściwości, oraz RecyclerView

--- app/src/main/java/com/example/user/pum_lab2/Place.java
+++ app/src/main/java/com/example/user/pum_lab2/Place.java
@@ -5,4 +5,32 @@ package com.example.user.pum_lab2;
  */
 
 public class Place {
+
+    private String placeName;
+    private int temperature;
+    private String information;
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
 }
